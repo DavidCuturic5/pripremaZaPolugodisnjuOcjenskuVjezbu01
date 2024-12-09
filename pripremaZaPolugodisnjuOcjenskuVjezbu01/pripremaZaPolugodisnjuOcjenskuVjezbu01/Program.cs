66801ce1457f92d8c57b43f3f6c4d3dcceb1d4ad
DavidCuturic5 <david.cuturic5@gmail.com>
2024-12-09 12:43:30 +0100

    Add files via upload

---
+++ pripremaZaPolugodisnjuOcjenskuVjezbu01/pripremaZaPolugodisnjuOcjenskuVjezbu01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaPolugodisnjuOcjenskuVjezbu01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Unesite vrijednost stranice x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Unesite vrijednost stranice y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Unesite vrijednost stranice z:");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+
+            if(x <= 0 || y <= 0 || z <= 0)
+            {
+                Console.WriteLine("Stranice moraju biti veće od 0");
+            }
+
+
+            if((x*x+y*y == z*z) || (x*x+z*z==y*y) || (y * y + z * z == x * x))
+            {
+                Console.WriteLine("Trokut je pravokutan");
+            }
+            else { Console.WriteLine("Trokut nije pravokutan"); }
+
+            Console.ReadKey();  
+
+
+        }
+    }
+}
